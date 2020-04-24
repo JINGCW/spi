35b1862e17d55cce38355f334a9b24e2e369bca6
JINGCW <1562359870@qq.com>
2020-04-24 13:40:23 +0800

    rm

--- helpers_test.py
+++
@@ -1,7 +0,0 @@
-from spiderman.spiders.helpers import urls_according_keywords
-
-if __name__ == '__main__':
-    out = urls_according_keywords(total_pages=8)
-    print(len(out))
-    for o in out:
-        print(o)
--- test.py
+++
@@ -1,9 +0,0 @@
-import getopt
-import sys
-
-help = 'baidu_crawler.py -k <keyword> [-t <timeout> -p <total pages>]'
-print(sys.argv[1:])
-opts, args = getopt.getopt(sys.argv[1:], "hk:t:p:")
-print(opts)
-print("---------------------")
-print(args)
